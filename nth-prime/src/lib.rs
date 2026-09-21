1d8207364e68ab5dece3d22cd448227c028a6e37
rauw <barash.gad@s.afeka.ac.il>
2026-09-21 14:08:02 +0300

    Simplify nth-prime primality check
    Negate the divisibility test instead of using an empty if branch.

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -4,12 +4,11 @@ pub fn nth(n: u32) -> u32 {
     primes.push(2);
     let mut cnt = 3;
     while primes.len() as u32 <= n {
-        if primes
+        if !primes
             .iter()
             .take_while(|&p| p * p <= cnt)
             .any(|&p| cnt % p == 0)
         {
-        } else {
             primes.push(cnt);
         }
         cnt += 2;
